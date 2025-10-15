b16dc5a24c910393046d32d1febdbdfd33720712
carlsmagana <carlsmagana@hotmail.com>
2025-10-15 11:44:28 -0600

    Clean backend: remove all Entity Framework dependencies and conflicting files

--- proteia-backend/Program-minimal.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-
-var app = builder.Build();
-
-app.MapGet("/", () => "Proteia API is running! " + DateTime.Now);
-app.MapGet("/health", () => new { status = "ok", time = DateTime.Now });
-
-app.Run();
